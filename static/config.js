cba8320fd4485f41df84f64a81db3fb1b0a16237
windworst <windworst@gmail.com>
2020-03-04 11:59:33 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
 function configData() {
   return {
-    "author" : "小霸王&babyblue, 丢盔弃甲, Everglow, 搬砖妹, 洋洋&高楠, siga, 加菲, Carrera, <a href='https://ephen.me/'>灰姑娘</a>, 阿喀琉斯, stone"
+    "author" : "小霸王"//&babyblue, 丢盔弃甲, Everglow, 搬砖妹, 洋洋&高楠, siga, 加菲, Carrera, <a href='https://ephen.me/'>灰姑娘</a>, 阿喀琉斯, stone"
   };
 }
